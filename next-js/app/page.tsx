c17e30b917dd44b976b4b9f6614ac71bcb9f9093
oluoko <brianotieno586@gmail.com>
2024-10-30 23:17:59 +0300

    Pawn Hub: Chessboard.tsx component

--- next-js/app/page.tsx
+++ next-js/app/page.tsx
@@ -1,3 +1,9 @@
+import Chessboard from "@/components/Chessboard";
+
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className="w-screen h-screen bg-[rgb(33,38,45)] text-[#b1bac4]">
+      <Chessboard />
+    </div>
+  );
 }
---
+++ next-js/components/Chessboard.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+
+const Chessboard = () => {
+  return <div className="">I am a chessboard</div>;
+};
+
+export default Chessboard;
