3c7cacd9be2ed2900079ef16078cb5c5b1e9e035
oluoko <brianotieno586@gmail.com>
2024-11-28 03:31:41 +0300

    Pawn Hub: Method to find all possible safe squares

--- angular-js/src/app/chess-logic/chess-board.ts
+++ angular-js/src/app/chess-logic/chess-board.ts
@@ -1,4 +1,4 @@
-import { Color, FENChar } from './models';
+import { Color, Coords, FENChar, SafeSquares } from './models';
 import { Bishop } from './pieces/bishop';
 import { King } from './pieces/king';
 import { Knight } from './pieces/knight';
@@ -152,5 +152,77 @@ export class ChessBoard {
     this.chessBoard[newX][newY] = newPiece;
 
     return isPositionSafe;
+  }
+
+  private findSafeSquares(): SafeSquares {
+    const safeSquares: SafeSquares = new Map<string, Coords[]>();
+    for (let x = 0; x < this.chessBoardSize; x++) {
+      for (let y = 0; y < this.chessBoardSize; y++) {
+        const piece: Piece | null = this.chessBoard[x][y];
+
+        // this might not be this._playerColor but this.playerColor
+        if (!piece || piece.color !== this._playerColor) continue;
+
+        const pieceSafeSquares: Coords[] = [];
+
+        for (const { x: dx, y: dy } of piece.directions) {
+          let newX: number = x + dx;
+          let newY: number = y + dy;
+
+          if (!this.areCoordsValid(newX, newY)) continue;
+
+          let newPiece: Piece | null = this.chessBoard[newX][newY];
+          if (newPiece && newPiece.color === piece.color) continue;
+
+          // need to restrict pawn movement in certain directions
+          if (piece instanceof Pawn) {
+            // cant move pawn two squares straight if there is a piece in front
+            if (dx === 2 || dx === -2) {
+              if (newPiece) continue;
+              if (this.chessBoard[newX + (dx === 2 ? -1 : 1)][newY]) continue;
+            }
+
+            // can't move pawn one square straight if there is a piece in front
+            if ((dx === 1 || dx === -1) && dy === 0 && newPiece) continue;
+
+            // can't move pawn diagonally if there is no piece there
+            if (
+              (dy === 1 || dy === -1) &&
+              (!newPiece || piece.color === newPiece.color)
+            )
+              continue;
+          }
+
+          if (
+            piece instanceof Pawn ||
+            piece instanceof Knight ||
+            piece instanceof King
+          ) {
+            if (this.isPositionSafeAfterMove(piece, x, y, newX, newY)) {
+              pieceSafeSquares.push({ x: newX, y: newY });
+            }
+          } else {
+            while (this.areCoordsValid(newX, newY)) {
+              newPiece = this.chessBoard[newX][newY];
+              if (newPiece && newPiece.color === piece.color) break;
+
+              if (this.isPositionSafeAfterMove(piece, x, y, newX, newY)) {
+                pieceSafeSquares.push({ x: newX, y: newY });
+              }
+
+              if (newPiece !== null) break;
+
+              newX += dx;
+              newY += dy;
+            }
+          }
+        }
+
+        if (pieceSafeSquares.length)
+          safeSquares.set(x + ',' + y, pieceSafeSquares);
+      }
+    }
+
+    return safeSquares;
   }
 }
